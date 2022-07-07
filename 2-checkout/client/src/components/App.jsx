ea8489754a3de6dcfdedaebdc5610b338d61bd6e
Patrick O'Shea <osheap81796@gmail.com>
2022-07-07 18:09:28 -0400

    Slight Optimization to App State

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -1,4 +1,3 @@
-//import React from "react";
 import HomePage from './HomePage.jsx';
 import Form1 from './Form1.jsx';
 import Form2 from './Form2.jsx';
@@ -7,41 +6,9 @@ import Confirmation from './Confirmation.jsx';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-//class App extends React.Component {
-//  constructor(props) {
-//    super(props)
-//    this.state = {
-//      sessionId: JSON.stringify(document.cookie, undefined, "\t").slice(6, -1),
-//      name: '',
-//      email: '',
-//      password: '',
-//      address1: '',
-//      address2: '',
-//      city: '',
-//      state: '',
-//      zipCode: '',
-//      phoneNum: '',
-//      CCNum: '',
-//      expiration: '',
-//      CVV: '',
-//      billingZip: '',
-//      activePage: 'HomePage',
-//      alreadyPurchased: false
-//    }
-//  }
-//
-//  componentDidMount() {
-//    axios.get('/checkout')
-//      .then((res) => {
-//        if (res) {
-//          this.setState({ alreadyPurchased: true })
-//        }
-//      })
-//  }
-
 const App = () => {
 
-  const [state, setState] = useState({
+  const [fields, setFields] = useState({
     sessionId: JSON.stringify(document.cookie, undefined, "\t").slice(6, -1),
     name: '',
     email: '',
@@ -55,24 +22,23 @@ const App = () => {
     CCNum: '',
     expiration: '',
     CVV: '',
-    billingZip: '',
-    activePage: 'HomePage',
-    alreadyPurchased: false
+    billingZip: ''
   })
+
+  const [activePage, setActivePage] = useState('HomePage');
+  const [alreadyPurchased, setAlreadyPurchased] = useState(false);
 
   useEffect(() => {
     axios.get('/checkout')
       .then((res) => {
         if (res) {
-          setState(previousState => {
-            return { ...previousState, alreadyPurchased: true }
-          });
+          setAlreadyPurchased(true);
         }
       })
   }, [])
 
   const handleChange = e => {
-    setState(previousState => {
+    setFields(previousState => {
       return { ...previousState, [e.target.id]: e.target.value }
     });
   }
@@ -80,9 +46,7 @@ const App = () => {
   const buttonHandler = e => {
     e.preventDefault();
     let nextPage = e.target.getAttribute('next');
-    setState(previousState => {
-      return { ...previousState, activePage: nextPage }
-    });
+    setActivePage(nextPage);
   }
 
   const purchaseButtonHandler = () => {
@@ -92,26 +56,24 @@ const App = () => {
   }
 
   const sendData = () => {
-    let userData = state;
-    delete userData['activePage'];
-    delete userData['alreadyPurchased'];
+    let userData = fields;
     return axios.post('/checkout', userData)
       .then((res) => res)
       .catch((err) => console.error(err))
   }
 
 
-  switch (state.activePage) {
+  switch (activePage) {
     case 'HomePage':
-      return <HomePage buttonHandler={buttonHandler} alreadyPurchased={state.alreadyPurchased} />;
+      return <HomePage buttonHandler={buttonHandler} alreadyPurchased={alreadyPurchased} />;
     case 'Form1':
-      return <Form1 buttonHandler={buttonHandler} handleChange={handleChange} appState={state} />;
+      return <Form1 buttonHandler={buttonHandler} handleChange={handleChange} appState={fields} />;
     case 'Form2':
-      return <Form2 buttonHandler={buttonHandler} handleChange={handleChange} appState={state} />;
+      return <Form2 buttonHandler={buttonHandler} handleChange={handleChange} appState={fields} />;
     case 'Form3':
-      return <Form3 buttonHandler={buttonHandler} handleChange={handleChange} appState={state} />;
+      return <Form3 buttonHandler={buttonHandler} handleChange={handleChange} appState={fields} />;
     case 'Confirmation':
-      return <Confirmation buttonHandler={buttonHandler} purchaseButtonHandler={purchaseButtonHandler} appState={state} />
+      return <Confirmation buttonHandler={buttonHandler} purchaseButtonHandler={purchaseButtonHandler} appState={fields} />
     default:
       return <span>Error: Invalid Active Page</span>
 
